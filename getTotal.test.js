7a259f84f285322bbae735f86e118c01a4b2ac2d
edgar-orange <EDGAR_AGUIRRE@homedepot.com>
2020-06-30 13:33:16 -0500

    tdd example

---
+++ getTotal.test.js
@@ -0,0 +1,20 @@
+const getTotal = (array) => {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return sum;
+};
+
+describe('getTotal()', () =>  {
+    test('returns the toal sum of the array', () => {
+        //Arange
+        const input = [1, 2, 3, 4, 5 ];
+        const expectedOut = 15; 
+        //Act
+        const result = getTotal(input);
+
+        //Assert 
+        expect(result).toEqual(expectedOut);
+    });
+})
